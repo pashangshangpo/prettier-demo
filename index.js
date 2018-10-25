6b5eb352ac9e8fffc5a1784cb4dd2f9999de4d8c
爬上上坡 <pashangshangpo@gmail.com>
2018-10-25 12:00:50 +0800

    5

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-console.log( 12)
+console.log(12);
 
-function c(a){
+function c(a) {
   console.log(1);
 }
 
-if(1) {
-  console.log(1)
+if (1) {
+  console.log(1);
 }
-alert(111);  
+alert(111);
